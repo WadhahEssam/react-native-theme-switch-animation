6ec4b8765c641f0aef3ed9c3f0c6ab1092b3ba1d
Wadah Esam <wadah-wleed@hotmail.com>
2023-11-04 00:47:46 +0300

    chore: lint

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -24,8 +24,6 @@ export default function App() {
             animationConfig: {
               type: 'fade',
               duration: 900,
-              cyRatio: -0.5,
-              cxRatio: -0.9,
             },
           });
         }}
