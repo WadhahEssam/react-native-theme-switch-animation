a0af8a831bb110ec93b323a7df9f243e4b453942
Wadah Esam <wadah-wleed@hotmail.com>
2023-11-18 17:38:13 +0300

    chore: better header

--- ios/ThemeSwitchAnimationModule.h
+++ ios/ThemeSwitchAnimationModule.h
@@ -1,22 +1,12 @@
-#ifdef RCT_NEW_ARCH_ENABLED
-
-#import "RNThemeSwitchAnimationModule.h"
 #import <React/RCTEventEmitter.h>
 
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNThemeSwitchAnimationModule.h"
 @interface ThemeSwitchAnimationModule : RCTEventEmitter <NativeThemeSwitchAnimationModuleSpec, CAAnimationDelegate>
-
-@property (nonatomic, strong) UIImageView *overlayView;
-@property (nonatomic, assign) BOOL isAnimating;
-
-- (void)captureAndDisplayScreen;
-- (void)displayCapturedImageFullScreen:(UIImage *)image;
-- (UIImage *)captureScreen;
-
 #else
 #import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-
 @interface ThemeSwitchAnimationModule : RCTEventEmitter <RCTBridgeModule, CAAnimationDelegate>
+#endif
 
 @property (nonatomic, strong) UIImageView *overlayView;
 @property (nonatomic, assign) BOOL isAnimating;
@@ -24,7 +14,6 @@
 - (void)captureAndDisplayScreen;
 - (void)displayCapturedImageFullScreen:(UIImage *)image;
 - (UIImage *)captureScreen;
-#endif
 
 @end
 
