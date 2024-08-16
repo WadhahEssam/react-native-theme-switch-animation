1cc20dac3a6087fb331eaaed60e77dc911f6b67e
Wadah Esam <wadah-wleed@hotmail.com>
2024-08-16 15:06:40 +0300

    fix: keyWindow depreactin warning

--- ios/ThemeSwitchAnimationModule.mm
+++ ios/ThemeSwitchAnimationModule.mm
@@ -80,14 +80,18 @@ RCT_EXPORT_METHOD(unfreezeScreen: (NSString *)type duration:(double)duration cxR
 }
 
 - (void)displayCapturedImageFullScreen:(UIImage *)image {
+    NSLog(@"here bere");
     overlayView = [[UIImageView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     overlayView.image = image;
     overlayView.contentMode = UIViewContentModeScaleAspectFill;
-    [[UIApplication sharedApplication].keyWindow addSubview:overlayView];
+    UIWindow *keyWindow = [UIApplication sharedApplication].windows.firstObject;
+    if (keyWindow) {
+        [keyWindow addSubview:overlayView];
+    }
 }
 
 - (UIImage *)captureScreen {
-    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    UIWindow *keyWindow = [UIApplication sharedApplication].windows.firstObject;
     CGRect rect = [keyWindow bounds];
     UIGraphicsBeginImageContextWithOptions(rect.size,YES,0.0f);
     CGContextRef context = UIGraphicsGetCurrentContext();
